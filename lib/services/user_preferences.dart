9fcfb99fc2164d7703c9fe4f5223d98ad9be2156
Hakim Gueye <beatsjordy@gmail.com>
2024-03-29 18:11:55 +0000

    Update Dashboard and UserPreferences files

--- lib/services/user_preferences.dart
+++ lib/services/user_preferences.dart
@@ -31,10 +31,6 @@ class UserPreferences {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.remove(_userIdKey);
     await prefs.remove(_supervisor);
-
-    await Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder:(context) => LoginPage()), (route) => false);
-
     Navigator.of(context).pushNamedAndRemoveUntil('/login', (route) => false);
-
   }
 }
