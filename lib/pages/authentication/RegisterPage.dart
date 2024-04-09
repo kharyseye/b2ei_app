25f2681791dce3bc756a0f19b2b1c408f0c566fa
Hakim Gueye <beatsjordy@gmail.com>
2024-04-09 10:58:13 +0000

    Update add user page and things

--- lib/pages/authentication/RegisterPage.dart
+++ lib/pages/authentication/RegisterPage.dart
@@ -35,7 +35,8 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Colors.green[200],
+        foregroundColor: Colors.white,
+        backgroundColor: PrimaryColor,
         title: Text('Ajouter un Utilisateur'),
       ),
       body: SingleChildScrollView(
@@ -44,14 +45,14 @@ class _RegisterPageState extends State<RegisterPage> {
             Stack(
               children: [
                 Container(
-                  /*width: w,
+                    /*width: w,
                   height: h * 0.2,
                   decoration: BoxDecoration(
                       image: DecorationImage(
                           image: AssetImage("assets/images/ban3.jpg"),
                           fit: BoxFit.cover)
                   ),*/
-                ),
+                    ),
                 DelayedAnimation(
                     delay: 500,
                     child: Container(
@@ -207,7 +208,6 @@ class _RegisterPageState extends State<RegisterPage> {
                           )
                         ]),
                   ),
-
                   Container(
                     decoration: BoxDecoration(
                         color: Colors.white,
@@ -338,7 +338,7 @@ class _RegisterPageState extends State<RegisterPage> {
                                     fontSize: 18,
                                   )),
                               description:
-                                  Text('Oups une erreur est survenue !',
+                                  Text('Oups une erreur est survenue ! $e',
                                       style: TextStyle(
                                         color: Colors.white,
                                         fontSize: 15,
--- lib/pages/superior_interface/add_user.dart
+++ lib/pages/superior_interface/add_user.dart
@@ -1,6 +1,11 @@
+import 'package:b2ei_app/constant.dart';
 import 'package:b2ei_app/pages/authentication/RegisterPage.dart';
+import 'package:b2ei_app/services/user_preferences.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 import '../../model/Users.dart';
 
@@ -12,130 +17,136 @@ class AddUserPage extends StatefulWidget {
 }
 
 class _Request_empState extends State<AddUserPage> {
-
   final users = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        foregroundColor: Colors.white,
         title: Text("GESTION DES UTILISATEURS"),
-        backgroundColor: Colors.green[200],
+        backgroundColor: PrimaryColor,
       ),
-      body: Stack(
+      body: Column(
         children: [
           //BOUTTON AJOUTER UN UTILISATEUR !!!!!!!
-          Positioned(
-            top: 40.0,
-            right: 10.0,
-            child: ElevatedButton.icon(
-              onPressed: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => RegisterPage(),
-                    ));
-
-              },
-              icon: Icon(Icons.add),
-              label: Text('Ajouter un Utilisateur',
-              style: TextStyle(
-                fontSize: 18,
+          Gap(12.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RegisterPage(),
+                      ));
+                },
+                icon: Icon(Icons.add),
+                label: Text(
+                  'Ajouter un Utilisateur',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
               ),
-              ),
-            ),
+            ],
           ),
-          Positioned(
-            top: 150,
-            left: 0,
-            right: 0,
-            bottom: 0,
-              child:StreamBuilder(
-                stream: FirebaseFirestore.instance.collection("users").snapshots(),
-                builder:(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
-                  if(snapshot.connectionState == ConnectionState.waiting){
-                    return CircularProgressIndicator();
-                  }
-                  if(!snapshot.hasData){
-                    return Text("Aucun utilisateur");
-                  }
-                  List<Users> users = [];
-                  snapshot.data!.docs.forEach((data) {
-                    users.add(Users.fromData(data));
-                  });
-                  return  ListView.builder(
-                    itemCount: users.length,
-                    itemBuilder: (context, index){
-                      var id_user = snapshot.data!.docs[index].id;
-                      final user = users[index];
-                      final email = user.email;
-                      final username = user.username;
-                      final uid = user.uid;
-                      final department = user.department;
-                      final supervisor = user.supervisor;
+          Gap(4.0),
+          Expanded(
+            child: StreamBuilder(
+              stream:
+                  FirebaseFirestore.instance.collection("users").snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                }
+                if (!snapshot.hasData) {
+                  return Text("Aucun utilisateur");
+                }
+                List<Users> users = [];
+                snapshot.data!.docs.forEach((data) {
+                  users.add(Users.fromData(data));
+                });
+                return ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    var id_user = snapshot.data!.docs[index].id;
+                    final user = users[index];
+                    final username = user.username;
+                    if (user.supervisor) return SizedBox();
 
-                      return Card(
-                        child: ListTile(
-                          //leading: Image.asset("assets/images/history.jpg"),
-                          title: Text("$username",style: TextStyle(
+                    return Card(
+                      child: ListTile(
+                        //leading: Image.asset("assets/images/history.jpg"),
+                        title: Text(
+                          "$username",
+                          style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 24,
                           ),
-                          ),
-                          trailing: Wrap(
-                            spacing: -16,
-                            children: [
-
-                              IconButton(
-                                icon: const Icon(
-                                  Icons.delete,
-                                  color: Colors.redAccent,
-                                  size: 35,
-                                ),
-                                onPressed: () async {
-                                  showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: Text('Confirmation'),
-                                        content: Text('Êtes-vous sûr de vouloir supprimer cet utilisateur ?'),
-                                        actions: <Widget>[
-                                          TextButton(
-                                            onPressed: () {
-                                              Navigator.of(context).pop();
-                                            },
-                                            child: Text('Annuler'),
-                                          ),
-                                          TextButton(
-                                            onPressed: () async {
-                                              await FirebaseFirestore.instance
-                                                  .collection("users")
-                                                  .doc(id_user)
-                                                  .delete();
-                                              ScaffoldMessenger.of(context).showSnackBar(
-                                                SnackBar(
-                                                  content: Text('Utilisateur supprimé avec succès'),
-                                                ),
-                                              );
-                                              Navigator.of(context).pop();
-                                            },
-                                            child: Text('Supprimer'),
-                                          ),
-                                        ],
-                                      );
-                                    },
-                                  );
-                                },
-                              ),
-
-                            ],
-                          ),
                         ),
-                      );
-                    },
-                  );
-                } ,
-              )
+                        trailing: Wrap(
+                          spacing: -16,
+                          children: [
+                            IconButton(
+                              icon: const Icon(
+                                Icons.delete,
+                                color: Colors.redAccent,
+                                size: 35,
+                              ),
+                              onPressed: () async {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Text('Confirmation'),
+                                      content: Text(
+                                          'Êtes-vous sûr de vouloir supprimer cet utilisateur ?'),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text('Annuler'),
+                                        ),
+                                        TextButton(
+                                          onPressed: () async {
+                                            await FirebaseFirestore.instance
+                                                .collection('auth')
+                                                .doc(id_user)
+                                                .delete();
+
+                                            await FirebaseFirestore.instance
+                                                .collection("users")
+                                                .doc(id_user)
+                                                .delete();
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(
+                                              SnackBar(
+                                                content: Text(
+                                                    'Utilisateur supprimé avec succès'),
+                                              ),
+                                            );
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text('Supprimer'),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
           ),
         ],
       ),
